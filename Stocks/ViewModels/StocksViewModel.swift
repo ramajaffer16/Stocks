14c6cfb0c2745bbfbd909dec794301895d16dfdd
Ramadhan <ramajaffer16@gmail.com>
2024-07-16 19:16:15 +0300

    new_commits

--- Stocks/ViewModels/StocksViewModel.swift
+++ Stocks/ViewModels/StocksViewModel.swift
@@ -10,27 +10,35 @@ import RxSwift
 
 
 class StocksViewModel{
-// Declaring the Service class
-private let stocksService: StocksAPIService
-
-//Declaring the publish subjects
-//Input publishsubject from either trigger or a button that will trigger the fetch
-let triggerfetch = PublishSubject<Void>()
-
-//Output publishsubject that will emit the observable
-let stocksAPIData = PublishSubject<Any>()
-
-//Creating the initializer
-init(stocksService: StocksAPIService = StocksAPIService()){
-    self.stocksService = stocksService
-
-
-
-triggerfetch.flatMapLatest {_ in
-    StocksAPIService.fetchAPI()
+    // Declaring the Service class
+    private let stocksService: StocksAPIService
     
-}
+    //Declaring the publish subjects
+    //Input publishsubject from either trigger or a button that will trigger the fetch
+    let triggerfetch = PublishSubject<Void>()
     
+    //Output publishsubject that will emit the observable
+    let stocksAPIData = PublishSubject<Any>()
+    
+    //Creating the initializer
+    init(stocksService: StocksAPIService = StocksAPIService()){
+        self.stocksService = stocksService
+        
+        
+        
+        triggerfetch.flatMapLatest {_ in
+            stocksService.fetchAPI()
+                .catch{error in
+                    print ("Error occured")
+                    return Observable.just([])
+                }
+        }
+        
+        .bind(to: stocksAPIData)
+        .disposed(by: DisposeBag)
+        }
+    }
+
 
 
 
